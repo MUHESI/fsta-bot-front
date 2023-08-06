d399fa61152d65292030ac8bddf511cc86ceb7e6
MUHESI <mvmmuhesi@gmail.com>
2023-08-06 18:05:07 +0200

    fix: some changes

--- src/pages/loading/index.tsx
+++ src/pages/loading/index.tsx
@@ -3,20 +3,7 @@ import React from "react";
 function LoadingPage() {
   return (
     <div className="flex items-center justify-center h-full w-full">
-      <p>
-        Loading pppp Enthusiastically architect transparent content without
-        scalable communities. Objectively matrix exceptional communities through
-        highly efficient infomediaries. Dramatically transform cross functional
-        intellectual capital without standardized imperatives. Compellingly
-        parallel task multidisciplinary networks vis-a-vis client-centered
-        growth strategies. Efficiently engineer intermandated resources whereas
-        cross-platform potentialities. Objectively reinvent B2B convergence with
-        market-driven imperatives. Interactively foster e-business web services
-        without customer directed content. Completely utilize interoperable
-        mindshare before worldwide core competencies. Energistically engineer
-        principle-centered ROI for dynamic e-services. Monotonectally deliver
-        inexpensive architectures through business supply.
-      </p>
+      <p className="text-center">Loading</p>
     </div>
   );
 }
